cab80a904882eb963d2aa965ee7a83dcddb1cf91
Adam <Adam@sigterm.info>
2020-01-20 17:00:10 -0500

    Remove npcs with no combat level

--- npcs.py
+++ npcs.py
@@ -4,6 +4,7 @@ import mwparserfromhell as mw
 import api
 import util
 from typing import *
+import copy
 
 def run():
     npcs = {}
@@ -20,13 +21,19 @@ def run():
                 doc = util.get_doc_for_id_string(name + str(vid), version, npcs)
                 if doc == None:
                     continue
-                for key in ["name", "hitpoints", "combat"]:
-                    util.copy(key, doc, version, lambda x: x)
+                util.copy("name", doc, version, lambda x: x)
+                for key in ["hitpoints", "combat"]:
+                    util.copy(key, doc, version, lambda x: int(x))
 
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
             print("NPC {} failed:".format(name))
             traceback.print_exc()
+
+    for npcId in copy.copy(npcs):
+        npc = npcs[npcId]
+        if not 'combat' in npc:
+            del npcs[npcId]
 
     util.write_json("npcs.json", "npcs.min.json", npcs)
