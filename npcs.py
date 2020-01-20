ee1db436b69873a774bd8d8b29eea2caa1402518
Adam <Adam@sigterm.info>
2020-01-20 12:50:39 -0500

    Add npc dumper

---
+++ npcs.py
@@ -0,0 +1,40 @@
+import traceback
+import re
+import mwparserfromhell as mw
+import api
+import util
+from typing import *
+import copy
+
+
+def run():
+	npcs = {}
+
+	npc_pages = api.query_category("Monsters")
+	for name, page in npc_pages.items():
+		if name.startswith("Category:"):
+			continue
+
+		try:
+			code = mw.parse(page, skip_style_tags=True)
+
+			for (vid, version) in util.each_version("Infobox Monster", code):
+				doc = util.get_doc_for_id_string(name + str(vid), version, npcs)
+				if doc == None:
+					continue
+				util.copy("name", doc, version, lambda x: x)
+				for key in ["hitpoints", "combat"]:
+					util.copy(key, doc, version, lambda x: int(x))
+
+		except (KeyboardInterrupt, SystemExit):
+			raise
+		except:
+			print("NPC {} failed:".format(name))
+			traceback.print_exc()
+
+	for npcId in copy.copy(npcs):
+		npc = npcs[npcId]
+		if not 'combat' in npc:
+			del npcs[npcId]
+
+	util.write_json("npcs.json", "npcs.min.json", npcs)
